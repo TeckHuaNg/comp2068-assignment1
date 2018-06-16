4c3c054e90aec364bf0723b043a99edd7eb1c3f6
TeckHuaNg <200348264@student.georgianc.on.ca>
2018-06-15 20:19:01 -0400

    Third try

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -66,10 +66,11 @@ exports.send = (req, res) => {
   // create reusable transporter object using the default SMTP transport
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
+    // port: 465,
+    // secure: true, // true for 465, false for other ports
     // requireTLS: true,
     auth: {
+      type: 'login',
       user: email,
       pass: connect,
     },
